bbfec9b589fc1adebae805c6ac37d25d266ecdce
seohyeon125 <wiseohyeonlove@gmail.com>
2026-08-26 00:45:07 +0900

    Add files via upload

---
+++ 연습/연습하깅/p02.py
@@ -0,0 +1,14 @@
+a = 100
+b = 50
+result = a + b
+print(a, '+', b, '=', result)  # 출력: 100 + 50 = 150
+
+result = a - b
+print(a, '+', b, '=', result)
+
+result = a * b
+print(a, '+', b, '=', result)
+
+result = a / b
+print(a, '+', b, '=', result)
+exit()  # 프로그램 종료
